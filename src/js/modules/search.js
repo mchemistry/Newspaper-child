f41e59e2e861afeffdb816bfad164e2d918e7c45
Tân Dv <mchemistry95@gmail.com>
2021-05-13 17:39:02 +0700

    Update search component
    update

---
+++ src/js/modules/search.js
@@ -0,0 +1,8 @@
+
+export default class InputWithSearch {
+    constructor(inputClassName, tableId, suggestOptions) {
+        this.inputClassName = inputClassName;
+        this.tableId = tableId;
+        this.suggestOptions = suggestOptions;
+    }
+}
