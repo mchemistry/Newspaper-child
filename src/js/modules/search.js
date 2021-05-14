d945fb4a026e4eb5db8be8bb14ac83c4b8703ab3
Tân Dv <mchemistry95@gmail.com>
2021-05-14 17:56:27 +0700

    Update

--- src/js/modules/search.js
+++ src/js/modules/search.js
@@ -7,30 +7,36 @@ export default class InputWithSearch {
         this.suggestOptions = suggestOptions;
     }
 
-    countRow = () => {
-        return $(`#${this.tableId} tr:not(:nth-child(1)):visible`).length;
-    }
+  countRow = () => {
+      return $(`#${this.tableId} tr:not(:nth-child(1)):visible`).length;
+  };
 
-    showSuggestTxt = () => {
-        $('#suggest-txt').show(100);
-        $('.search-result').hide();
-    }
+  showSuggestTxt = () => {
+      $('#suggest-txt').show(50);
+      $('.search-result').hide();
+  };
 
-    innerResult = (numOfResult, inputTxt) => {
-        $('#suggest-txt').hide(100);
-        $('.search-result').show().text(`Co ${numOfResult} ket qua voi tu khoa "${inputTxt}"`);
-    }
+  innerResult = (numOfResult, inputTxt) => {
+      $('#suggest-txt').hide(100);
+      $('.search-result')
+          .show()
+          .html(`Có ${numOfResult} kết quả với từ khóa <strong>"${inputTxt}"</strong>`);
+  };
 
-    search = () => {
-        const _self = this;
-        $(`.${_self.inputClassName}`).on('keyup', function () {
-            const originalInputVal = $(this).val();
-            const inputVal = originalInputVal.toLowerCase();
-            // eslint-disable-next-line array-callback-return
-            $(`#${_self.tableId} tr:not(:nth-child(1))`).filter(function () {
-                $(this).toggle($(this).text().toLowerCase().indexOf((inputVal)) > -1);
-            });
-            if (originalInputVal !== '') { _self.innerResult(_self.countRow(), originalInputVal); } else { _self.showSuggestTxt(); }
-        });
-    }
+  search = () => {
+      const _self = this;
+      $(`.${_self.inputClassName}`).on('keyup', function () {
+          const originalInputVal = $(this).val();
+          const inputVal = originalInputVal.toLowerCase();
+          // eslint-disable-next-line array-callback-return
+          $(`#${_self.tableId} tr:not(:nth-child(1))`).filter(function () {
+              $(this).toggle($(this).text().toLowerCase().indexOf(inputVal) > -1);
+          });
+          if (originalInputVal !== '') {
+              _self.innerResult(_self.countRow(), originalInputVal);
+          } else {
+              _self.showSuggestTxt();
+          }
+      });
+  };
 }
