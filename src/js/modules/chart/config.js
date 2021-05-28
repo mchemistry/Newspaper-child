ef160bc1d70a71f3422d8b4a7b7691eca95ced0b
Tân Dv <mchemistry95@gmail.com>
2021-05-28 22:19:34 +0700

    feat: add timeline to charts

--- src/js/modules/chart/config.js
+++ src/js/modules/chart/config.js
@@ -1,31 +1,62 @@
 
 // <block:config:0>
-
 import Utils from './utils';
-// import { possitiveColor, negativeColor } from './const';
+import { possitiveColor, negativeColor } from './const';
 
 const data = (items) => {
-    const labels = items.map(item => Utils.filterDate(item['Ngày']));
-    const dataGold = items.map(item => Number(Utils.filterNumber(item['Mua/Bán(tấn)'])));
-    const finalData = [];
-    for (let i = 0; i < labels.length; i++) {
-        finalData.push([labels[i], dataGold[i]]);
-    }
-    return finalData;
+    console.log(items);
+    const result = items.map((item) => {
+        const date = Utils.filterDate(item['Ngày']);
+        // console.log(date);
+        const value = Utils.filterNumber(item['Mua/Bán(tấn)']);
+        return Object.assign(date, value);
+    });
+    return result;
 };
 
+const vi = [{
+    name: 'vi',
+    options: {
+        months: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7',
+            'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+        shortMonths: ['Thg 1', 'Thg 2', 'Thg 3', 'Thg 4', 'Thg 5', 'Thg 6', 'Thg 7',
+            'Thg 8', 'Thg 9', 'Thg 10', 'Thg 11', 'Thg 12'],
+        days: ['Chủ Nhật', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'],
+        shortDays: ['CN', 'Th2', 'Th3', 'Th4', 'Th5', 'Th6', 'Th7'],
+        toolbar: {
+            download: 'Tải ảnh',
+            selection: 'Selection',
+            selectionZoom: 'Selection Zoom',
+            zoomIn: 'Zoom In',
+            zoomOut: 'Zoom Out',
+            pan: 'Panning',
+            reset: 'Reset Zoom',
+        }
+    }
+}];
 
 const config = (items, setting) => {
     const finalData = data(items);
+    const values = finalData.map(value => value.y);
+    // console.log(values);
     // console.log(finalData);
-    // const max = Math.ceil(Math.max(...finalData.datasets[0].data) + setting.num);
-    // const min = Math.ceil(Math.min(...finalData.datasets[0].data) - setting.num);
+    const max = Math.ceil(Math.max(...values) + setting.num);
+    const min = Math.ceil(Math.min(...values) - setting.num);
+    // console.log(`${max} + ${min}`);
     return {
         // eslint-disable-next-line no-unused-vars
-        // colors: [({ value, seriesIndex, w }) => (value <= 0 ? negativeColor : possitiveColor)],
+        colors: [({ value, seriesIndex, w }) => (value <= 0 ? negativeColor : possitiveColor)],
         chart: {
+            defaultLocale: 'vi',
+            locales: vi,
+            height: 380,
+            width: 680,
             type: 'bar',
-            d: 'area-datetime',
+            animations: {
+                initialAnimation: {
+                    enabled: false
+                }
+            }
         },
         plotOptions: {
             bar: {
@@ -59,12 +90,13 @@ const config = (items, setting) => {
         }],
         xaxis: {
             type: 'datetime',
-            min: new Date('01 Mar 2012').getTime(),
-            tickAmount: 6,
+            // labels: {
+            //     format: 'dd/MM',
+            // }
         },
-        // yaxis: {
-        //     max, min
-        // }
+        yaxis: {
+            max, min
+        }
     };
 };
 
--- src/js/modules/chart/utils.js
+++ src/js/modules/chart/utils.js
@@ -1,8 +1,15 @@
 
 const Utils = {
     // convertDataToColor: number => (number >= 0 ? possitiveColor : negativeColor),
-    filterDate: date => new Date(date.replace(/[Date()]/g, '').split(',').reverse().join('/')).getTime(),
-    filterNumber: number => Number(number),
+    filterDate: (date) => {
+        let temp = date.replace(/[Date()]/g, '').split(',').reverse();
+        temp = [temp[1], temp[0], temp[2]];
+        // console.log(temp.join('/'));
+        return {
+            x: `${temp.join('/')}`
+        };
+    },
+    filterNumber: number => ({ y: Number(number) }),
 };
 
 export default Utils;
